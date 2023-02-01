2953ba932edbd6153344c3d79fbe89cfe92b1b9c
parzi <santos.delaft@gmail.com>
2023-02-01 14:08:04 -0600

    erased another migration

--- db/migrate/20230130172335_regenerate_join_table_part_numbers_scraped_data.rb
+++
@@ -1,8 +0,0 @@
-class RegenerateJoinTablePartNumbersScrapedData < ActiveRecord::Migration[7.0]
-  def change
-    create_join_table :part_numbers, :scraped_data do |t|
-      t.index :part_number
-      t.index :scraped_data
-    end
-  end
-end
