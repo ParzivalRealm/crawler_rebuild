0bae6e585453b7382fad6d029011d7983f8d47fc
parzi <santos.delaft@gmail.com>
2023-02-01 13:58:29 -0600

    changed migration file

--- db/migrate/20230124165553_create_attachments.rb
+++ db/migrate/20230124165553_create_attachments.rb
@@ -1,7 +1,7 @@
 class CreateAttachments < ActiveRecord::Migration[7.0]
   def change
     create_table :attachments do |t|
-      t.string :attachment_id polymorphic: true, index: true
+      t.references :attachment, polymorphic: true, index: true
       t.string :attachment_type
       t.string :file
       t.string :file_type
