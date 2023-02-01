c9f972f371da042568ce79abecd64eb4d4c7f65e
parzi <santos.delaft@gmail.com>
2023-02-01 14:12:36 -0600

    changed price data type to match psql

--- db/migrate/20230131195151_change_column_type_scraped_data.rb
+++ db/migrate/20230131195151_change_column_type_scraped_data.rb
@@ -1,6 +1,6 @@
 class ChangeColumnTypeScrapedData < ActiveRecord::Migration[7.0]
   def change
-    change_column :scraped_data, :price, :integer
-    change_column :scraped_data, :inventory, :integer
+    change_column :scraped_data, :price, 'decimal USING CAST(price AS decimal)'
+    change_column :scraped_data, :inventory, 'integer USING CAST(inventory AS integer)'
   end
 end
