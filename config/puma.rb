b6155242a76e17d7d03dd1c40a6f6229f7f51ef4
parzi <santos.delaft@gmail.com>
2023-02-25 11:02:39 -0600

    added daemonize to puma file

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ application_path = Rails.root
 railsenv = ENV['RAILS_ENV'] || 'production'
 directory application_path
 environment railsenv
-#daemonize true
+daemonize true
 pidfile "#{application_path}/tmp/pids/puma-#{railsenv}.pid"
 state_path "#{application_path}/tmp/pids/puma-#{railsenv}.state"
 stdout_redirect
