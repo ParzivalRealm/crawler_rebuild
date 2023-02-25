701222cb6b018076d7ae98e0c0e91073c5198441
parzi <santos.delaft@gmail.com>
2023-02-25 11:57:21 -0600

    removed stdouterr log

--- config/puma.rb
+++ config/puma.rb
@@ -12,8 +12,6 @@ environment railsenv
 daemonize true
 pidfile "#{application_path}/tmp/pids/puma-#{railsenv}.pid"
 state_path "#{application_path}/tmp/pids/puma-#{railsenv}.state"
-stdout_redirect
-"#{application_path}/log/puma-#{railsenv}.stdout.log"
-"#{application_path}/log/puma-#{railsenv}.stderr.log"
+
 threads 0, 16
 bind "unix://#{application_path}/tmp/sockets/#{railsenv}.socket"
